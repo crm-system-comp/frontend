a28090169bb5a97b863d5955ecdcc43e650576d4
oliceglad <vycheslavofficial@gmail.com>
2025-05-21 18:22:21 +0400

    added admin

---
+++ src/api/adminApi.js
@@ -0,0 +1,49 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getSession } from "../../session";
+
+export const adminApi = createApi({
+  reducerPath: "adminApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `http://localhost:8080/api/admin`,
+    prepareHeaders: (headers) => {
+      const token = getSession()?.accessToken;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["Users", "Orders"],
+  endpoints: (builder) => ({
+    getAllUsers: builder.query({
+      query: () => "/users",
+      providesTags: ["Users"],
+    }),
+    getAllOrders: builder.query({
+      query: () => "/orders",
+      providesTags: ["Orders"],
+    }),
+    updateOrder: builder.mutation({
+      query: ({ orderId, data }) => ({
+        url: `/orders/${orderId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Orders"],
+    }),
+    deleteOrder: builder.mutation({
+      query: (orderId) => ({
+        url: `/orders/${orderId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Orders"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllUsersQuery,
+  useGetAllOrdersQuery,
+  useUpdateOrderMutation,
+  useDeleteOrderMutation,
+} = adminApi;
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,16 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { ordersApi } from "../api/orders";
 import { authApi } from "../api/auth";
+import { adminApi } from "../api/adminApi";
 
 export const store = configureStore({
   reducer: {
     [ordersApi.reducerPath]: ordersApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [adminApi.reducerPath]: adminApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(ordersApi.middleware)
-      .concat(authApi.middleware),
+      .concat(authApi.middleware)
+      .concat(adminApi.middleware),
 });
 
 setupListeners(store.dispatch);
